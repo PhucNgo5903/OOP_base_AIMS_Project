09e8f4a49a3363c876baad4e6d387dc7394772b8
phucngo <phucngo@gmail.com>
2024-11-25 23:47:41 +0700

    Lab03 Finished

---
+++ AimsProject/src/hust/soict/hedspi/test/disc/DigitalVideoDiscWrapper.java
@@ -0,0 +1,11 @@
+package hust.soict.hedspi.test.disc;
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class DigitalVideoDiscWrapper {
+    DigitalVideoDisc dvdPhucNH;
+    DigitalVideoDiscWrapper(DigitalVideoDisc dvdPhucNH) {
+        super();
+
+        this.dvdPhucNH = dvdPhucNH;
+    }
+}
--- AimsProject/src/hust/soict/hedspi/test/disc/TestPassingParameter.java
+++ AimsProject/src/hust/soict/hedspi/test/disc/TestPassingParameter.java
@@ -6,10 +6,19 @@ public class TestPassingParameter {
         // Create two DigitalVideoDisc objects with titles "Jungle PhucNH" and "Cinderella PhucNH"
         DigitalVideoDisc jungleDVDPhucNH = new DigitalVideoDisc("Jungle PhucNH");
         DigitalVideoDisc cinderellaDVDPhucNH = new DigitalVideoDisc("Cinderella PhucNH");
+
+        DigitalVideoDiscWrapper wjungleDVDPhucNH = new DigitalVideoDiscWrapper(jungleDVDPhucNH);
+        DigitalVideoDiscWrapper wcinderellaDVDPhucNH = new DigitalVideoDiscWrapper(cinderellaDVDPhucNH);
+
         // Attempt to swap the two DVD objects and print result
         swap(jungleDVDPhucNH, cinderellaDVDPhucNH);
         System.out.println("Ngo Hoang Phuc 20225903. Jungle dvd title: " + jungleDVDPhucNH.getTitlePhucNH());
         System.out.println("Ngo Hoang Phuc 20225903. Cinderella dvd title: " + cinderellaDVDPhucNH.getTitlePhucNH());
+
+        // Correct swap() function
+        swap(wjungleDVDPhucNH, wcinderellaDVDPhucNH);
+        System.out.println("Ngo Hoang Phuc 20225903. Correct swapped jungle dvd title: " + wjungleDVDPhucNH.dvdPhucNH.getTitlePhucNH());
+        System.out.println("Ngo Hoang Phuc 20225903. Correct swapped cinderella dvd title: " + wcinderellaDVDPhucNH.dvdPhucNH.getTitlePhucNH());
 
         // Change the title of "jungleDVDPhucNH" to the title of "cinderellaDVDPhucNH"
         changeTitle(jungleDVDPhucNH, cinderellaDVDPhucNH.getTitlePhucNH());
@@ -17,11 +26,18 @@ public class TestPassingParameter {
         System.out.println("Ngo Hoang Phuc 20225903. Jungle dvd title: " + jungleDVDPhucNH.getTitlePhucNH());
     }
 
-    // This method attempts to swap the two objects o1PhucNH and o2PhucNH
+    //This method attempts to swap the two objects o1PhucNH and o2PhucNH
     public static void swap(Object o1PhucNH, Object o2PhucNH) {
         Object tmpPhucNH = o1PhucNH;
         o1PhucNH = o2PhucNH;
         o2PhucNH = tmpPhucNH;   
+    }
+
+    // Correct swap() function
+    public static void swap(DigitalVideoDiscWrapper o1PhucNH, DigitalVideoDiscWrapper o2PhucNH) {
+        DigitalVideoDisc tmpPhucNH = o1PhucNH.dvdPhucNH;
+        o1PhucNH.dvdPhucNH = o2PhucNH.dvdPhucNH;
+        o2PhucNH.dvdPhucNH = tmpPhucNH;
     }
 
     // This method changes the title of a given DigitalVideoDisc object to a new title
